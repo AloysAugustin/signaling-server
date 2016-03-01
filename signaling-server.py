dc762f7fb3ffa60bf3d6c1d26a3acf8dd0978cd6
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-03-01 01:33:24 +0100

    Fix indentation

--- signaling-server.py
+++ signaling-server.py
@@ -9,67 +9,67 @@ import websockets
 clients = {}
 
 def new_id():
-	testId = ''.join(random.choice('0123456789abcdef') for i in range(16))
-	while testId in clients:
-		testId = ''.join(random.choice('0123456789abcdef') for i in range(16))
-	return testId
+    testId = ''.join(random.choice('0123456789abcdef') for i in range(16))
+    while testId in clients:
+        testId = ''.join(random.choice('0123456789abcdef') for i in range(16))
+    return testId
 
 def hello_message(clientId):
-	d = {
-		"type" : "hello",
-		"id" : clientId,
-		"peers" : [i for i in clients],		# For testing purposes only
-	}
-	return json.dumps(d)
+    d = {
+        "type" : "hello",
+        "id" : clientId,
+        "peers" : [i for i in clients],     # For testing purposes only
+    }
+    return json.dumps(d)
 
 def data_message(sourceId, data):
-	d = {
-		"type": "data",
-		"src" : sourceId,
-		"data" : data,
-	}
-	return json.dumps(d)
+    d = {
+        "type": "data",
+        "src" : sourceId,
+        "data" : data,
+    }
+    return json.dumps(d)
 
 def error_message(description, info):
-	d = {
-		"type" : "error",
-		"desc" : description,
-		"info" : info,
-	}
-	return json.dumps(d)
+    d = {
+        "type" : "error",
+        "desc" : description,
+        "info" : info,
+    }
+    return json.dumps(d)
 
 async def handler(websocket, path):
-	global clients
+    global clients
 
-	clientId = new_id()
-	print("New client, ID: {}".format(clientId))
-	clients[clientId] = websocket
-	await websocket.send(hello_message(clientId))
+    clientId = new_id()
+    print("New client, ID: {}".format(clientId))
+    clients[clientId] = websocket
+    await websocket.send(hello_message(clientId))
 
-	while True:
-		try:
-			message = await websocket.recv()
-			data = json.loads(message)
-			if "type" in data:
-				if data["type"] == "data" and "dest" in data and "data" in data:
-					if data["dest"] in clients:
-						try:
-							await clients[data["dest"]].send(data_message(clientId, data["data"]))
-						except websockets.exceptions.ConnectionClosed:
-							if data["dest"] in clients:
-								del clients[data["dest"]]
-							await websocket.send(error_message("client_unavailable", data["dest"]))
-					else:
-						await websocket.send(error_message("client_unavailable", data["dest"]))
-		except websockets.exceptions.ConnectionClosed:
-			if clientId in clients:
-				del clients[clientId]
-			break
+    while True:
+        try:
+            message = await websocket.recv()
+            data = json.loads(message)
+            if "type" in data:
+                if data["type"] == "data" and "dest" in data and "data" in data:
+                    if data["dest"] in clients:
+                        try:
+                            await clients[data["dest"]].send(data_message(clientId, data["data"]))
+                        except websockets.exceptions.ConnectionClosed:
+                            if data["dest"] in clients:
+                                del clients[data["dest"]]
+                            await websocket.send(error_message("client_unavailable", data["dest"]))
+                    else:
+                        await websocket.send(error_message("client_unavailable", data["dest"]))
+        except websockets.exceptions.ConnectionClosed:
+            if clientId in clients:
+                del clients[clientId]
+            break
 
 def run_server():
-	start_server = websockets.serve(handler, '0.0.0.0', 6000)
-	asyncio.get_event_loop().run_until_complete(start_server)
-	asyncio.get_event_loop().run_forever()
+    start_server = websockets.serve(handler, '0.0.0.0', 6000)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
 
 if __name__ == '__main__':
-	run_server()
+    run_server()
--- test_client.py
+++ test_client.py
@@ -8,38 +8,38 @@ import asyncio
 import websockets
 
 def data_message(dest, data):
-	d = {
-		"type" : "data",
-		"dest" : dest,
-		"data" : data,
-	}
-	return json.dumps(d)
+    d = {
+        "type" : "data",
+        "dest" : dest,
+        "data" : data,
+    }
+    return json.dumps(d)
 
 async def client_handler():
-	async with websockets.connect('ws://localhost:6000/') as websocket:
-		hellomsg = await websocket.recv()
-		hellodata = json.loads(hellomsg)
-		print("Received hello. My ID is {}".format(hellodata["id"]))
-		print("{} peers available.".format(len(hellodata["peers"])))
-		# Send 10 messages at 10s interval and then exit. Print messages received in the mean time
-		for i in range(10):
-			end = time.time() + 5.0
-			while True:
-				try:
-					msg = await asyncio.wait_for(asyncio.ensure_future(websocket.recv()), timeout=end - time.time())
-					msgdata = json.loads(msg)
-					if "type" in msgdata:
-						if msgdata["type"] == "data":
-							print("Received {} from {}.".format(msgdata["data"], msgdata["src"]))
-						elif msgdata["type"] == "error":
-							print("Error: client {} is not available anymore".format(msgdata["info"]))
-				except asyncio.TimeoutError:
-					break
-			if len(hellodata["peers"]) > 0:
-				dest = random.choice(hellodata["peers"])
-				print("Sending to {}".format(dest))
-				await websocket.send(data_message(dest, "blah"))
+    async with websockets.connect('ws://localhost:6000/') as websocket:
+        hellomsg = await websocket.recv()
+        hellodata = json.loads(hellomsg)
+        print("Received hello. My ID is {}".format(hellodata["id"]))
+        print("{} peers available.".format(len(hellodata["peers"])))
+        # Send 10 messages at 10s interval and then exit. Print messages received in the mean time
+        for i in range(10):
+            end = time.time() + 5.0
+            while True:
+                try:
+                    msg = await asyncio.wait_for(asyncio.ensure_future(websocket.recv()), timeout=end - time.time())
+                    msgdata = json.loads(msg)
+                    if "type" in msgdata:
+                        if msgdata["type"] == "data":
+                            print("Received {} from {}.".format(msgdata["data"], msgdata["src"]))
+                        elif msgdata["type"] == "error":
+                            print("Error: client {} is not available anymore".format(msgdata["info"]))
+                except asyncio.TimeoutError:
+                    break
+            if len(hellodata["peers"]) > 0:
+                dest = random.choice(hellodata["peers"])
+                print("Sending to {}".format(dest))
+                await websocket.send(data_message(dest, "blah"))
 
 if __name__ == '__main__':
-	asyncio.get_event_loop().set_debug(True)
-	asyncio.get_event_loop().run_until_complete(client_handler())
+    asyncio.get_event_loop().set_debug(True)
+    asyncio.get_event_loop().run_until_complete(client_handler())
